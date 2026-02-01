b5db5aa1040ec68c5dd6028a4cb3cc6180eedce0
mohdalees <aleeskhan63@gmail.com>
2026-02-01 12:06:56 +0530

    commit initial

--- server/index.js
+++ server/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./db");
-const authroute = require("./routes/authroute");
+const authRoutes = require("./routes/authroute");
 
-
-dotenv.config();
 connectDB();
 
 const app = express();
@@ -13,14 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// REGISTER ROUTES HERE 👇
-app.use("/api/auth", require("./routes/authroute"));
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("LocalEase API is running 🚀");
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
