b688c16cdb1a33f14667b9d2c365792cd351ebed
mohdalees <aleeskhan63@gmail.com>
2026-02-10 11:04:29 +0530

    initial.commit

--- local-service-client/src/components/Navbar.jsx
+++ local-service-client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-green-700 p-6 text-purple-100 flex justify-between ">
-      <h1 className="text-2xl font-bold">LocalEase</h1>
+      <h1 className="text-2xl font-bold">LSP</h1>
       <div className="space-x-3 text-lg font-bold">
         <Link className="hover:text-teal-800 transition duration-300 ease-in-out" to="/">Home</Link>
         <Link className="hover:text-teal-800 transition duration-300 ease-in-out" to="/booking">Booking</Link>
